a8d01841edcf0ee741d855d006391cf5af9b99c6
kmajkows <261715@pwr.edu.pl>
2022-07-26 15:55:29 +0200

    Added adder

---
+++ src/test/java/CalculationTest.java
@@ -0,0 +1,18 @@
+import org.example.Calculator;
+import org.junit.Test;
+
+public class CalculationTest {
+
+    @Test
+    public void additionTest() {
+
+        Calculator calc = new Calculator();
+
+        int x = 2;
+        int y = 3;
+        assert calc.add(x, y) == 5;
+    }
+
+
+
+}
